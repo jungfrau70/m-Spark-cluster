f709de540defb39a90e6ff547776d0632c8b1c67
InHwan Jung <inhwan.jung@gmail.com>
2022-04-03 12:50:10 -0700

    Updated

--- remove-git-rm-cached.sh
+++ remove-git-rm-cached.sh
@@ -3,5 +3,5 @@ git rm --cached Step3_setup_cluster/5_Spark/hive-postgres-data/ -r
 git rm --cached Step3_setup_cluster/A_MongoDB/mongodb-data/journal/ -r
 git rm --cached Step3_setup_cluster/A_MongoDB/mongodb-data/ -r
 git rm --cached workspace/3_Kafka/trips/yellow_tripdata_2021-01.csv
-git rm --cached workspace/workspace/logs/ -r
+git rm --cached workspace/logs/ -r
 #git rm --cached workspace/9_MLOps -r
